50cb5249ee7b5483aa1222283bafb2cbbcead0a2
Javier Zaleta Martínez <94091554+j-cordz@users.noreply.github.com>
2023-02-02 04:09:30 +0000

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,36 @@
+const { WebClient } = require('@slack/web-api');
+const axios = require('axios').default;
+const images = {
+  "morning": "https://cloud-5y6hx4kva-hack-club-bot.vercel.app/0exploration-cabin.png",
+  "afternoon": "https://cloud-5y6hx4kva-hack-club-bot.vercel.app/1forest-astronaut.png",
+  "night": "https://cloud-5y6hx4kva-hack-club-bot.vercel.app/2grassland-robot.png"
+} 
+async function setPFP() {
+  var hour = new Date().getHours() - 6
+  let image
+  if (5 < hour && hour < 12) {
+    image = await axios.get(images.morning, {
+      responseType: "arraybuffer",
+    });
+  }
+  else if (12 < hour && hour < 20) {
+    image = await axios.get(images.afternoon, {
+      responseType: "arraybuffer",
+    });
+  }
+  else {
+    image = await axios.get(images.night, {
+      responseType: "arraybuffer",
+    });
+  }
+  const client = new WebClient();
+  const slackRequest = await client.users.setPhoto({
+    image: image.data,
+    token: process.env.SLACK_TOKEN
+  });
+}
+
+export default async (req, res) => {
+  await setPFP()
+  res.send("Started changing your PFP!")
+}
