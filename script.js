1d4a592966de22e1dd8de12c8812b5bdafc02d55
Javier Zaleta Martínez <94091554+j-cordz@users.noreply.github.com>
2023-05-01 22:43:42 -0600

    Update script.js easter egg

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const consoleMessage = `
-Created by Javier Zaleta Martínez
-"color: #fff; background: hsl(0, 93%, 45%); padding:5px 0;"
-`;
-console.log(consoleMessage);
+const consoleMessage = "Created by Javier Zaleta Martínez";
+const consoleStyles =
+  "color: #fff; background: hsl(0, 93%, 45%); padding: 5px 0;";
+console.log(consoleMessage, consoleStyles);
